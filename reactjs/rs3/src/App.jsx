fc4f15ecf7afe5425a60671612d32eebe4e40628
sabitumar68 <sabitumar68@gmail.com>
2024-09-26 11:59:18 +0600

    rs3

---
+++ reactjs/rs3/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+var sname="home"
+var object="fname:Ismail;lname:Hossain;"
+var object1="firstname:Wahida;lastname:Rumi;"
+function App() {
+
+
+  return (
+    <>
+    <p>pass DaTa</p>
+<h1>Hello</h1>
+
+      <p>I am from Bangladesh</p>
+      <p>My school name is {sname}</p>
+      <p>My father name is {object}</p>
+      <p>My mother name is {object1}</p>
+          </>
+  )
+}
+
+export default App
---
+++ reactjs/rsintro/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h2>Hello World</h2><br></br>
+    <h1>My score is {10*8}</h1>
+    
+    </>
+  )
+}
+
+export default App

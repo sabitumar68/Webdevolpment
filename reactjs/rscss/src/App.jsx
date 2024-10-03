0f0dd337ff03a05edd22a5c7f4cf0e0c4046936c
sabitumar68 <sabitumar68@gmail.com>
2024-10-03 12:06:02 +0600

    rscss

---
+++ reactjs/rscss/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+<h1>Sabit Umar</h1>
+    <p>Sabit is a very good boy.He lives in Uttara Sector-13,Road-8,House-33.His father Name is Ismail Hossain.
+       His mother Name is Wahida Rumi. He is 12 years old.He is a very good coder he loves coding.He finished 
+       father is a bussinasman he is a Freelenseer and his mother is House Wife.</p>
+    </>
+  )
+}
+
+export default App
